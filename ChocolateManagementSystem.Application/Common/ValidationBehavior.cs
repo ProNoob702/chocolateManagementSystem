544a31fad6a419554e9b2ec29211264596772317
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2023-04-26 22:31:19 +0100

    ValidatorBehavior with TRequest notnull only

--- ChocolateManagementSystem.Application/Common/ValidationBehavior.cs
+++ ChocolateManagementSystem.Application/Common/ValidationBehavior.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace ChocolateManagementSystem.Application.Common;
 
-public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>  where TRequest : notnull
 {
     private readonly IEnumerable<IValidator<TRequest>> _validators;
 
@@ -14,13 +14,21 @@ public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-         if (_validators.Any())
+        if (_validators.Any())
         {
             var context = new ValidationContext<TRequest>(request);
-            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
-            if (failures.Count != 0)
-                throw new FluentValidation.ValidationException(failures);
+
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults
+                .Where(r => r.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
         }
         return await next();
     }
