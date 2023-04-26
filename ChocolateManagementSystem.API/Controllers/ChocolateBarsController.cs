0bd0d663198fad59f70aaa2aba729851129215c5
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2023-04-26 18:41:33 +0100

    ChocolateBarsController

---
+++ ChocolateManagementSystem.API/Controllers/ChocolateBarsController.cs
@@ -0,0 +1,41 @@
+﻿using ChocolateManagementSystem.Application.Features.ChocolateBars.CreateChocolateBar;
+using ChocolateManagementSystem.Application.Features.ChocolateBars.DeleteChocolateBar;
+using ChocolateManagementSystem.Application.Features.ChocolateBars.GetAllChocolateBarsByFactory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChocolateManagementSystem.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChocolateBarsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ChocolateBarsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        var response = await _mediator.Send(new GetChocolateBarsByFactoryQuery());
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateChocolateBar(CreateChocolateBarCommand payload)
+    {
+        var newlyCreatedChocolateId = await _mediator.Send(payload);
+        return Ok(newlyCreatedChocolateId);
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteChocolateBar(DeleteChocolateBarCommand payload)
+    {
+        await _mediator.Send(payload);
+        return NoContent();
+    }
+}
